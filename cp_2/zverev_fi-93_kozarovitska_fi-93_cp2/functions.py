9f3844f6373367fafba9c548a3cc8b1f69538bc7
Polina <polinaitsme@gmail.com>
2022-04-05 16:23:28 +0200

    'added two decyphering methods'

--- cp_2/zverev_fi-93_kozarovitska_fi-93_cp2/functions.py
+++ cp_2/zverev_fi-93_kozarovitska_fi-93_cp2/functions.py
@@ -1,15 +1,133 @@
-def get_blocks(y, r):
-    Y = []
-    n = len(y)
+from viginer import Viginer
+
+
+def get_blocks(text, r):
+    Y=[]
+    n = len(text)
     i = 0
-    while True:
-        if i + r >= n :
-            Y.append(y[i:])
-            break
-        else:
-            Y.append(y[i:i+r])
-        i += r
+    for i in range(r):
+        y = []
+        Y.append(y)
+
+    for i in range(n):
+        index = i % r
+        if text[i]!= '\n':
+            Y[index].append(text[i])
+
+
     return Y
 
+def get_D(r, y):
+    D = 0
+    for i in range (1,len(y)-r):
+        if (y[i] == y[i+r]):
+            D += 1
+    return D
+
+def get_r(y):
+    max = 0
+    r = 0
+    for i in range (6,30):
+        D = get_D(i, y)
+        if D > max:
+            max =D
+            r = i
+    return r
+
+def get_key_w_M(text):
+    r = get_r(text)
+    Y = get_blocks(text,r)
+    v = Viginer()
+    dict_frequency = v.count_frequency(text, 'rus')
+    #print(dict_frequency)
+    arr_frequency = []
+    for elem in dict_frequency:
+        arr_frequency.append(dict_frequency[elem])
+    k = []
+
+    for y in Y:
+        dict_frequency = v.count_frequency(y, 'rus')
+        arr_frequency = []
+        for elem in dict_frequency:
+            arr_frequency.append(dict_frequency[elem])
+        M = [0] * len(Y)
+        max_g = 0
+        for g in range(32):
+            sum = 0
+            for t in range (32):
+                sum += list(v.dict_russian_freq.items())[t][1]*arr_frequency[(t+g)%32]
+            if sum >  M[Y.index(y)]:
+                max_g = g
+                M[Y.index(y)] = sum
+        k.append(max_g)
+    return k
+
+def get_key_w_freq(text):
+    r = get_r(text)
+    Y = get_blocks(text, r)
+    v = Viginer()
+    k= []
+    print(v.dict_rus_alph)
+    for y in Y:
+        dict_freq = v.count_frequency(y, 'rus')
+        max = 0
+        letter_o = ''
+        for elem in dict_freq:
+            if dict_freq[elem] > max:
+                max = dict_freq[elem]
+                letter_o = elem
+        k_i = (v.russian_alphabet.index(letter_o)-8)%32
+        k.append(k_i)
+    return (k)
+
+def code_test(text):
+    v = Viginer()
+    print(v.dict_rus_alph)
+    arr = []
+    for elem in text:
+        ind = v.russian_alphabet.index(elem)
+        arr.append(ind)
+    return arr
+
+def decode_text(arr):
+    v =  Viginer()
+
+    result = ''
+    for elem in arr:
+        result+=v.russian_alphabet[elem]
+    return result
+
+def get_orig_text(key_arr, text_arr):
+    r = len(key_arr)
+    orig_arr = []
+    for i in range (len(text_arr)):
+        if i == 20:
+            a = 1
+            pass
+        ind = i%r
+        new_elem = (text_arr[i]- key_arr[ind])%32
+        orig_arr.append(new_elem)
+    orig_text = decode_text( orig_arr)
+    return orig_text
 
 
+f = open('cc', encoding = "UTF-8")
+text = f.read()
+f.close()
+
+
+#arr = get_key_w_freq(text)
+#key = decode_text(arr)
+#print(key)
+
+key = 'улановсеребряныепули'
+key_arr = code_test(key)
+print(key_arr)
+key_arr1= get_key_w_M(text)
+print(key_arr1)
+#text_arr =code_test(text)
+#txt = get_orig_text(key_arr, text_arr)
+#for i in range(len(txt)):
+    #print(txt[i], end='')
+
+#print(code_test(text))
--- cp_2/zverev_fi-93_kozarovitska_fi-93_cp2/viginer.py
+++ cp_2/zverev_fi-93_kozarovitska_fi-93_cp2/viginer.py
@@ -1,9 +1,17 @@
 class Viginer:
-    def __init__(self, text):
+    def __init__(self, text='фбвнффрвшішвршіфріщ'):
         self.alphabet = 'абвгґдеєжзиіїйклмнопрстуфхцчшщьюя '
+        self.russian_alphabet = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
+        self.dict_russian_freq = {'а': 0.07998, 'б': 0.01592, 'в': 0.04533, 'г': 0.01687, 'д': 0.02977, 'е': 0.08483,  'ж':0.0094, 'з': 0.01641, 'и': 0.07367, 'й': 0.01208, 'к': 0.03486, 'л': 0.04343, 'м': 0.03203, 'н': 0.067, 'о': 0.10983, 'п': 0.02804, 'р': 0.04746, 'с': 0.05473, 'т': 0.06318, 'у': 0.02615, 'ф': 0.00267, 'х': 0.00966, 'ц': 0.00486, 'ч': 0.0145, 'ш': 0.00718, 'щ': 0.00361, 'ъ': 0.00037, 'ы': 0.01898, 'ь': 0.01735, 'э': 0.00331, 'ю': 0.00639, 'я': 0.02001, ' ': 0.145}
+        self.arr_russ_freq = []
+        for elem in self.dict_russian_freq:
+            self.arr_russ_freq.append(self.dict_russian_freq[elem])
         self.dict_alph = {}
         for elem in self.alphabet:
             self.dict_alph[elem] = self.alphabet.index(elem)
+        self.dict_rus_alph = {}
+        for elem in self.russian_alphabet:
+            self.dict_rus_alph[elem] = self.russian_alphabet.index(elem)
         self.open_text = text
         self.n = len(self.open_text)
         self.arr_open = []
@@ -27,12 +35,18 @@ class Viginer:
             string_sypher+= letter
         return (arr_sypher, string_sypher)
 
-    def count_frequency(self, y):
+    def count_frequency(self, y, alph = 'ukr'):
         dict_y_frequency = {}
-        for elem in self.alphabet:
-            dict_y_frequency[elem] = 0
+        if alph == 'rus':
+            alph = self.russian_alphabet
+        else:
+            alph = self.alphabet
+        for elem in alph:
+            if elem != '\n':
+                dict_y_frequency[elem] = 0
         for i in y:
-            dict_y_frequency[i] += 1
+            if i != '\n':
+                dict_y_frequency[i] += 1
         return dict_y_frequency
 
     def count_I(self, y):
