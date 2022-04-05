babe523732eb4b232c189c911c78a2b76fada9bf
Polina <polinaitsme@gmail.com>
2022-04-05 09:51:01 +0200

    'added class viginer & few functions'

--- cp_2/zverev_fi-93_kozarovitska_fi-93_cp2/main.py
+++ cp_2/zverev_fi-93_kozarovitska_fi-93_cp2/main.py
@@ -1,16 +1,46 @@
-# This is a sample Python script.
+import functions as f
+from viginer import Viginer
+'''
+alphabet = 'абвгґдеєжзиіїйклмнопрстуфхцчшщьюя '
+dict_alph = {}
+for elem in alphabet:
+    dict_alph[elem] = alphabet.index(elem)
+print(dict_alph)
+open_text = 'фбвнфі'
+dict_open={}
+arr_open = []
+for elem in open_text:
+    index= dict_alph[elem]
+    dict_open[elem] = index
+    arr_open.append(index)
+print(dict_open)
+print(arr_open)
+k = 'абв'
+dict_k={}
+arr_k = []
+for elem in k:
+    index = dict_alph[elem]
+    dict_k[elem] = index
+    arr_k.append(index)
+print(dict_k)
+print(arr_k)
+r = len(k)
+n = len(open_text)
+dict_sypher = {}
+arr_sypher = []
+keys_open = list(dict_open.keys())
+keys_k = list(dict_k.keys())
+for i in range (n):
+    j = i %r
+    index = arr_open[i] + arr_k[j]
+    arr_sypher.append(index)
+print(arr_sypher)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+'''
+v = Viginer('фбвнффрвшішвршіфріщшвіфєвфврйцщшкгитьжфєжмтигршщушкгцамвщьіхцхвзлузауауауауауі')
+k = 'абві'
+res = v.cypher(k)
+arr_sypher = res[0]
+y = res[1]
+v.count_frequency(y)
+print(v.count_I(y))
